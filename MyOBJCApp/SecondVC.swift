64c073075a50ebe8aeacc395077012d059afaa74
nagas <n.agas@exus.co.uk>
2020-03-16 13:27:59 +0200

    finished bridging

---
+++ MyOBJCApp/SecondVC.swift
@@ -0,0 +1,30 @@
+//
+//  SecondVC.swift
+//  MyOBJCApp
+//
+//  Created by Nikolaos Agas on 3/16/20.
+//  Copyright © 2020 Nikolaos Agas. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
